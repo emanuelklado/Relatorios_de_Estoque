8e0ac0603c9c901c97247b7a61b8dc74ec83a35b
emanuelklado <emanuelklado@msn.com>
2023-01-16 23:21:28 -0300

    feat test: implementando o test de retorno do relatorio de produtos

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "roteador",
+        "brisanet",
+        "01-04-2020",
+        "02-06-2022",
+        78522456,
+        "local seco e longe de umidade",
+    )
+
+    assert str(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
