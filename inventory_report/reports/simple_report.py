c6177a04bbde9d59503d0ab671ab04787263e4a2
emanuelklado <emanuelklado@msn.com>
2023-01-16 21:37:37 -0300

    implementando a classe SimpleReport e seus metodos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,30 @@
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, list):
+        company = [product["nome_da_empresa"] for product in list]
+        old_date = cls.old_date(list)
+        expired_date = cls.expired_date(list)
+        more_product = max(set(company), key=company.count)
+
+        return (
+            f"Data de fabricação mais antiga: {old_date}\n"
+            f"Data de validade mais próxima: {expired_date}\n"
+            f"Empresa com mais produtos: {more_product}\n"
+        )
+
+    @classmethod
+    def old_date(cls, products):
+        dates = [product["data_de_fabricacao"] for product in products]
+        return min(dates)
+
+    @classmethod
+    def expired_date(cls, products):
+        dates = [
+            product["data_de_validade"]
+            for product in products
+            if product["data_de_validade"] > str(date.today())
+        ]
+        return min(dates)
